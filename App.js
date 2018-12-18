8afacda1302530ef0471bbd318013b4fa0fbb4b0
Joshua Stoltenberg <stoltenberg96@gmail.com>
2018-12-18 09:04:49 -0600

    Basic Todo App in RN

---
+++ App.js
@@ -0,0 +1,102 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView
+} from "react-native";
+
+let id = 0;
+
+const Todo = props => (
+  <View>
+    <Button onPress={props.onDelete} title="delete" />
+    <Text>{props.todo.text}</Text>
+  </View>
+);
+
+export default class App extends Component<Props> {
+  constructor() {
+    super();
+    this.state = {
+      todos: []
+    };
+  }
+
+  addTodo() {
+    const text = prompt("TODO text please!");
+    this.setState({
+      todos: [...this.state.todos, { id: id++, text: text, checked: false }]
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      })
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text>
+          Unchecked todo count:{" "}
+          {this.state.todos.filter(todo => !todo.checked).length}
+        </Text>
+        <Button onPress={() => this.addTodo()} title="Add TODO" />
+        <ScrollView>
+          {this.state.todos.map(todo => (
+            <Todo
+              onToggle={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
